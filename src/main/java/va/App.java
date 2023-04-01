01a8c6433807188d7fcdd6a6904d3cfbfe3dbb30
jaffleman <jaffleman@gmail.com>
2023-04-01 21:46:28 +0200

    projet docx_comparator

---
+++ src/main/java/va/App.java
@@ -0,0 +1,58 @@
+package va;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+public class App 
+{
+    public static void main( String[] args )throws IOException {
+        String report = "";
+        OperatingData initDataRequested = new OperatingData();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        Date date = new Date();
+        report += "Débuté à: "+simpleDateFormat.format(date);
+
+        File dir  = new File(initDataRequested.getPatn());
+        File[] listeOfFiles = dir.listFiles();
+        if (listeOfFiles == null){
+            report += "\nPathFile error:";
+            report += "\nSorry, no usable files in this directory!";
+            return;
+        }
+
+        System.out.println("\nCreation of the docx Class files...");
+        report += "\nDocx file :";
+        List <Docx> docxList = new ArrayList<>();
+        for(File file : listeOfFiles){
+            String fileName = file.getName();
+            if(fileName.endsWith(".docx")) docxList.add(new Docx(fileName, initDataRequested.getPatn()));
+        }
+
+        /***
+         * [a, b, c, d, e, f]
+         * pour chaque elem de Docx.shortNameListIndex
+         * rechercher une correspondance tant que index egale -1
+         * si diff de -1 stock tab
+         * 
+         *  
+        */
+        
+        List <String[]> associationList = new ArrayList<>();
+        report += "\nNumber of docx founded: "+docxList.size();
+        for (Docx docx : docxList) {
+            report += docx.getReport();
+        }
+
+        Date date2 = new Date();
+        long delta = date2.getTime()-date.getTime();
+        int minutes = (int)(delta/60000);
+        int seconds = (int)(delta/1000)-(minutes*60);
+        System.out.println(report);
+        System.out.println("\nTerminé en "+ minutes+" minutes "+seconds+" à: "+simpleDateFormat.format(date2));  
+    }
+}
